6630a7d0f2025052ef89c4d350aedfc4119fcf7b
sergeykin <sergeykinn@mail.ru>
2021-09-26 12:13:44 +0300

    не работало с componentscan

--- src/main/java/com/eugene/wsdemo/WsDemoApplication.java
+++ src/main/java/com/eugene/wsdemo/WsDemoApplication.java
@@ -3,11 +3,9 @@ package com.eugene.wsdemo;
 import com.eugene.wsdemo.repository.CountryRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackageClasses = CountryRepository.class)
-@ComponentScan(basePackages = {"com.eugene.wsdemo"})
 @SpringBootApplication
 public class WsDemoApplication {
 
